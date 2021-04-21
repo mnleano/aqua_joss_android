a9aa1fd33a728239fce7b27e46db30eab3731907
Dzapanta <dzapanta021@yahoo.com>
2021-04-21 22:00:45 +0800

    Cart data class

---
+++ app/src/main/java/com/ajws/aquajoss/data/view/CartProductView.kt
@@ -0,0 +1,8 @@
+package com.ajws.aquajoss.data.view
+
+data class CartProductView(
+    val id: Int,
+    val name: String,
+    val description: String,
+    val price: Double,
+    val quantity: Int)
